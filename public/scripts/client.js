40f44f424f6eb8cf8eea746d5559fb3703dc802a
Darren Lin <darrenlin815@hotmail.com>
2022-11-11 02:07:28 -0800

    day 3 commits

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,60 +2,59 @@
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+*/
 
 const renderTweets = function(data) {
 for (let i = data.length - 1; i >= 0; i--) {
   const newTweet = createTweetElement(data[i]);
-  $( '.tweet-container' ).append(newTweet);
+  $('#tweet-container').append(newTweet);
   }
 };
 
 const renderTweet = (data) => {
   const newTweet = createTweetElement(data);
-  $( 'tweet-container' ).prepend(newTweet);
+  $('#tweets-container').prepend(newTweet);
 };
 
-const createTweetElement = function(data) {
-let newTweet = `
-            <article class="tweet">
-              <header class="flex-container-row">
-                <div class="profile-pic-username flex-container-row">
-                  <img src="${data.user.avatars}"/>
-                  <p>${data.user.name}</p>
-                </div>
-                <p class="handle">${data.user.handle}</p>
-              </header>
-              <section class="tweet-content">
-                <p>${escape(data.content.text)}</p>
-              </section>
-              <footer>
-                <p class="time-created">${timeago.format(data.created_at)}</p>
-                <div>
-                  <i class="fas fa-flag"></i>
-                  <i class="fas fa-retweet"></i>
-                  <i class="fas fa-heart"></i>
-                </div>
-              </footer>
-            </article>
-            `;
-
-return newTweet;
+const createTweetElement = (data) => {
+  const newTweet = `
+                    <article class="tweet">
+                    <header class="flex-container-row">
+                      <div class="profile-pic-username flex-container-row">
+                        <img src="${data.user.avatars}"/>
+                        <p>${data.user.name}</p>
+                      </div>
+                      <p class="handle">${data.user.handle}</p>
+                    </header>
+                    <section class="tweet-content">
+                      <p>${escape(data.content.text)}</p>
+                    </section>
+                    <footer>
+                      <p class="time-created">${timeago.format(data.created_at)}</p>
+                      <div>
+                        <i class="fas fa-flag"></i>
+                        <i class="fas fa-retweet"></i>
+                        <i class="fas fa-heart"></i>
+                      </div>
+                    </footer>
+                    </article>
+                    `;
+  return newTweet;
 };
 
-const loadTweets = () => {
-  $.get('/tweets').then((data) => {
+const loadTweet = () => {
+  $.get('/tweets/').then((data) => {
     renderTweets(data);
-    $( '#tweet-text' ).val('');
+    $('#tweet-text').val('');
   });
 };
 
 const loadNewTweet = () => {
-  $.get('/tweets').then((data) => {
-    renderTweet(data[data.length -1]);
-    $( '#tweet-text' ).val('');
-    $( 'output.counter' ).val(140);
-  })
+  $.get('/tweets/').then((data) => {
+    renderTweet(data[data.length - 1]);
+    $('#tweet-text').val('');
+    $('output.counter').val(140);
+  });
 };
 
 const escape = function(str) {
@@ -65,21 +64,33 @@ const escape = function(str) {
 };
 
 const newTweetError = (message) => {
-  $( '#new-tweet-err-msg' ).text(message);
-  $( '#new-tweet-err' ).show();
+  $('#new-tweet-err-msg').text(message);
+  $('#new-tweet-err').show();
+  $('#tweet-text').focus();
 };
 
 const closeError = () => {
-  $( '#new-tweet-err' ).hide();
-  $( '#new-tweet-err-msg' ).text('');
+  $('#new-tweet-err').hide();
+  $('#new-tweet-err-msg').text('');
 };
 
-  $( document ).ready(function() {
+const toggleCreateTweet = () => {
+  $('#new-tweet-form').slideToggle();
+  $('#tweet-text').focus();
+};
 
-    $( '.new-tweet form' ).submit(function(event) {
+  $(document).ready(function() {
+
+    $('nav #compose-tweet-btn').click(function() {
+      toggleCreateTweet();
+    });
+
+    loadTweet();
+
+    $('.submit-and-display-button').submit(function(event) {
       event.preventDefault();
-      const tweetData = $( this ).serialize();
-      const charCount = Number($( 'output.counter' ).val());
+      const tweetData = $(this).serialize();
+      const charCount = Number($('output.counter').val());
       if (charCount === 140) {
         newTweetError('Cannot submit an empty tweet!');
         return;
@@ -88,8 +99,8 @@ const closeError = () => {
         return;
       }
 
-      $.post('/tweets', tweetData).then(() => {
-        loadNewTweets();
+      $.post('/tweets/', tweetData).then(() => {
+        loadNewTweet();
         closeError();
       });
     });
